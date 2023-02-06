887f1d062299730d6c60895fbe07cf8a841ea35a
dnieto1997 <daironieto6000@hotmail.com>
2023-02-06 09:55:20 -0500

    actualzacion

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
+import 'react-native-gesture-handler';
 import React from 'react'
+import { Text,View,SafeAreaView,StyleSheet } from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator } from '@react-navigation/stack';
+
+
+
+const Stack =createStackNavigator();
 
 const App = () => {
   return (
-    <div>
+    <>
+      <NavigationContainer>
+  <Stack.Navigator>
+     
+     <Stack.Screen>
       
-    </div>
+     </Stack.Screen>
+
+
+  </Stack.Navigator>
+
+
+
+
+      </NavigationContainer>
+    </>
   )
 }
-
 export default App
